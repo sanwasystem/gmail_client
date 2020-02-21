f6edcfc528287439609bd8b9584fac44a4103611
wak <wakabayashih@sanwasystem.com>
2020-02-21 13:49:19 +0900

    DynamoDBから取得したレコードのtype guardsをちゃんと書いた

--- src/getClient.ts
+++ src/getClient.ts
@@ -1,33 +1,20 @@
 import * as AWS from "aws-sdk";
-import * as toolbox from "ssc-aws-toolbox";
+import * as toolbox from "aws-toolbox";
 import GmailClient from "gmail-api-client";
 import * as env from "./env";
+import * as Types from "./types";
 
 const dynamo = new AWS.DynamoDB.DocumentClient({ region: env.defaultRegion });
 
-type DynamoDBRecord = {
-  access_token: string;
-};
-
-const isDynamoDBRecord = (arg: any): arg is DynamoDBRecord => {
-  if (typeof arg !== "object") {
-    return false;
-  }
-  if (typeof arg.access_token !== "string") {
-    return false;
-  }
-  return true;
-};
-
-const getClient = async () => {
+const getClient = async (key?: string): Promise<GmailClient> => {
   const accessToken = await toolbox.dynamo.getSingleRecord(
     dynamo,
-    env.DynamoDbTableName,
-    env.DynamoDbTableKeyName,
-    env.DynamoDbTableKeyValue,
-    isDynamoDBRecord
+    env.dynamoDbTableName,
+    env.dynamoDbTableKeyName,
+    key ?? env.defaultKeyValue,
+    Types.isDynamoDBRecord
   );
-  return new GmailClient(accessToken.access_token);
+  return new GmailClient(accessToken.Data.access_token);
 };
 
 export default getClient;
---
+++ src/types/index.ts
@@ -0,0 +1,67 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export type DynamoDBRecord = {
+  /**
+   * このレコードを一意に特定する文字列
+   */
+  Key: string;
+  /**
+   * この認証はどのアカウントで行われたかを書いておく。実際の処理には使われない
+   */
+  Account: string;
+  /**
+   * "Google"固定
+   */
+  Service: "Google";
+  /**
+   * トークン等
+   */
+  Data: {
+    access_token: string;
+    client_id: string;
+    client_secret: string;
+    refresh_token: string;
+    scope: string;
+    token_type: string;
+  };
+  /**
+   * 最終更新日時
+   */
+  Timestamp?: string;
+};
+
+export const isDynamoDBRecord = (arg: any): arg is DynamoDBRecord => {
+  if (arg === null || typeof arg !== "object") {
+    return false;
+  }
+  if (typeof arg.Key !== "string") {
+    return false;
+  }
+  if (typeof arg.Account !== "string") {
+    return false;
+  }
+  if (arg.Service !== "Google") {
+    return false;
+  }
+  if (arg.Data === null || typeof arg.Data !== "object") {
+    return false;
+  }
+  if (typeof arg.Data.access_token !== "string") {
+    return false;
+  }
+  if (typeof arg.Data.client_id !== "string") {
+    return false;
+  }
+  if (typeof arg.Data.client_secret !== "string") {
+    return false;
+  }
+  if (typeof arg.Data.refresh_token !== "string") {
+    return false;
+  }
+  if (typeof arg.Data.scope !== "string") {
+    return false;
+  }
+  if (typeof arg.Data.token_type !== "string") {
+    return false;
+  }
+  return true;
+};
